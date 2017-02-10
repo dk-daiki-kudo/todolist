2b6366f0cbac24f71adb3270d9527c1b9e701747
dk-daiki-kudo <daiki@blueipu.com>
2017-02-10 11:46:32 +0900

    port is changed

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.use(express.static('views'));
 app.use(bodyParser.json());
 
 //3000番ポートで待つ
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 
 //connection to database and get database object
 mongodb.MongoClient.connect("mongodb://heroku_d7sb4n2m:3rjg7an14o6kda7dm6d3nr07h1@ds145649.mlab.com:45649/heroku_d7sb4n2m", function(err, database) {
@@ -44,4 +45,22 @@ app.delete("/api/contents/:_id", function(req, res) {
   contents.remove({_id: mongodb.ObjectID(req.params._id)}, function() {
     res.send("delete");
   });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
